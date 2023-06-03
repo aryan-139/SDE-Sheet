9e0187049fa629ec7fd2dd3990d00f6fb8812538
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-03 21:25:04 +0530

    Create 242ValidAnagram.java

---
+++ String/242ValidAnagram.java
@@ -0,0 +1,20 @@
+//brute force first, 2 arrays used, store the characters, sort the array, if both equal, return true else false
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return(false);
+        int m[]= new int[s.length()];
+        int n[]=new int[t.length()];
+        for(int i=0;i<m.length;i++){
+            m[i]=(int)s.charAt(i); n[i]=(int)t.charAt(i);
+        }
+        Arrays.sort(m); Arrays.sort(n);
+        if(Arrays.equals(m,n))
+            return(true);
+        else
+            return(false);
+        
+    }
+}
+
